9856475280acd4022460c1e17c8742eaac786698
Mats Stalenhoef <3225222@student.mboutrecht.nl>
2019-05-08 14:03:51 +0200

    Nieuwe regel code toegevoegd

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-    echo "Dit is mijn eerste ervaring met Git";
+    echo "Dit is mijn eerste ervaring met Git<hr>";
+    echo "Dit is een nieuwe regel code<hr>";
 
 // user.name=Mats Stalenhoef
 // user.email=3225222@student.mboutrecht.nl
